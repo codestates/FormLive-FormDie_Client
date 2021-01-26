ebf8c3948ed9620f1536b8c992aeab080ba9383f
baekseungyong <pighorse61@gmail.com>
2021-01-26 16:13:58 +0900

    39-Suggestion UI 구현 / Suggestion UI 구현, Suggestion 기능 구현 및 history css 수정

---
+++ sagas/suggestion.ts
@@ -0,0 +1,29 @@
+import {
+	SUGGESTION_UPLOAD_REQUEST,
+	SUGGESTION_UPLOAD_SUCCESS,
+	SUGGESTION_UPLOAD_FAILURE,
+} from "../reducers/suggestion";
+import { all, call, fork, put, takeLatest } from "redux-saga/effects";
+import axios from "axios";
+
+export default function* suggestionSaga() {
+	function suggestionAPI(data) {
+		return axios
+			.post("/suggestion", data, {
+				withCredentials: true,
+				headers: { "Content-Type": "multipart/form-data" },
+			})
+			.then((result) => alert("파일 제출 완료되었습니다."));
+	}
+	function* suggestionFileRequest(action) {
+		try {
+			yield call(suggestionAPI, action.data);
+		} catch (e) {
+			console.log(e);
+		}
+	}
+	function* watchSuggestionFile() {
+		yield takeLatest(SUGGESTION_UPLOAD_REQUEST, suggestionFileRequest);
+	}
+	yield all([fork(watchSuggestionFile)]);
+}
