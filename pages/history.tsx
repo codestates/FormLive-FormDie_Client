bc864eaa1c56675534b5d46705ec381d14008efe
baekseungyong <pighorse61@gmail.com>
2021-01-27 20:07:08 +0900

    36-History 기능 구현 / 더보기 css 수정

--- pages/history.tsx
+++ pages/history.tsx
@@ -1,23 +1,41 @@
 import styles from "../styles/History.module.css";
 import SearchBar from "../components/SearchBar";
-import HistoryForm from "../containers/HistoryForm";
-import { HISTORY_LIST_REQUEST } from "../reducers/history";
+import HistoryCard from "../components/HistoryCard";
+import { IReducerState } from "../reducers";
+import {
+	HISTORY_LIST_REQUEST,
+	IHistoryReducerState,
+} from "../reducers/history";
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function History() {
 	const [ascPage, setAscPage] = useState<number>(1);
 	const [desPage, setDesPage] = useState<number>(1);
 	const [Query, setQuery] = useState<string>("");
 	const [Sort, setSort] = useState<string | null>(null);
+	const [ascMoreButton, setAscMoreButton] = useState<boolean>(false);
+	const [desMoreButton, setDesMoreButton] = useState<boolean>(true);
 	const dispatch = useDispatch();
+	const { historyTotal } = useSelector<IReducerState, IHistoryReducerState>(
+		(state) => state.history
+	);
+	const historyInfo = useSelector<IReducerState, IHistoryReducerState>(
+		(state) => state.history
+	);
+
+	const maxPage = Math.ceil(historyTotal / 10);
 
 	const onChangeAscendingSortHandler = () => {
-		setAscPage(1);
+		setDesMoreButton(false);
+		if (!ascMoreButton) {
+			setAscMoreButton(!ascMoreButton);
+		}
+
 		console.log("ascPage : ", ascPage);
 		console.log("onChangeAscendingSortHandler");
 		const params = {
-			page: ascPage,
+			page: 1,
 			sort: "asc",
 			q: Query,
 		};
@@ -31,9 +49,13 @@ export default function History() {
 	};
 
 	const onChangeDescendingSortHandler = () => {
-		setDesPage(1);
+		setAscMoreButton(false);
+		if (!desMoreButton) {
+			// maxPage 이후 클릭 시 버튼 다시 보이기
+			setDesMoreButton(!desMoreButton);
+		}
 		const params = {
-			page: desPage,
+			page: 1,
 			sort: null,
 			q: Query,
 		};
@@ -46,6 +68,42 @@ export default function History() {
 		setSort(null);
 	};
 
+	const onClick = () => {
+		console.log("maxPage : ", maxPage);
+		if (ascPage === maxPage) {
+			setAscMoreButton(!ascMoreButton);
+			alert("더 이상 불러올 것이 없습니다.");
+			setAscPage(1);
+		} else if (desPage === maxPage) {
+			setDesMoreButton(!desMoreButton);
+			alert("더 이상 불러올 것이 없습니다.");
+			setDesPage(1);
+		} else if (Sort === null) {
+			const params = {
+				page: desPage + 1,
+				sort: null,
+				q: "",
+			};
+
+			dispatch({
+				type: HISTORY_LIST_REQUEST,
+				data: params,
+			});
+			setDesPage(desPage + 1);
+		} else if (Sort === "asc") {
+			const params = {
+				page: ascPage + 1,
+				sort: "asc",
+				q: "",
+			};
+
+			dispatch({
+				type: HISTORY_LIST_REQUEST,
+				data: params,
+			});
+			setAscPage(ascPage + 1);
+		}
+	};
 	return (
 		<div className={styles.container}>
 			<header className={styles.header}>
@@ -72,13 +130,27 @@ export default function History() {
 
 				<SearchBar where={"history"} setQuery={setQuery} />
 			</header>
-			<HistoryForm
-				sort={Sort}
-				desPage={desPage}
-				setDesPage={setDesPage}
-				ascPage={ascPage}
-				setAscPage={setAscPage}
-			/>
+			<div className={styles.main}>
+				{historyInfo.historyList.map((el, idx) => (
+					<HistoryCard
+						groupId={el.groupId}
+						title={el.title}
+						updatedAt={el.updatedAt}
+						forms={el.forms}
+						key={idx}
+					/>
+				))}
+				{desMoreButton && (
+					<div onClick={onClick} className={styles.more}>
+						...
+					</div>
+				)}
+				{ascMoreButton && (
+					<div onClick={onClick} className={styles.more}>
+						...
+					</div>
+				)}
+			</div>
 		</div>
 	);
 }
