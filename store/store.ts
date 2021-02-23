3cbcf850164a354cb86e9e8d0d474c8bdf01619f
yeongbba <samm162@naver.com>
2021-02-23 20:45:54 +0900

    refactoring/redux saga와 persist 설정하기

--- store/store.ts
+++ store/store.ts
@@ -11,6 +11,12 @@ interface IStore extends Store {
   __PERSISTOR?: Persistor;
 }
 
+/*
+* Next.js에 saga와 persist 설정하기 위한 코드
+* persist 설정은 Form 관련 데이터만 적용되고, User 부분은 적용되지 않게 설정
+* persist 설정을 하면, 데이터가 localstorage에 저장되기 때문에
+* 새로고침을 해도 데이터가 유실되지 않음
+*/
 const reduxStore = (initialState) => {
   let store: IStore;
 
