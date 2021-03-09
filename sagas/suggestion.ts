8c4ecc1c52bd4ca337ce1ed88d28bb84b8cff692
baekseungyong <pighorse61@gmail.com>
2021-03-09 16:59:30 +0900

    리덕스에서 사용한 제너레이터 문법

--- sagas/suggestion.ts
+++ sagas/suggestion.ts
@@ -5,6 +5,13 @@ import {
 } from "../reducers/suggestion";
 import { all, call, fork, takeLatest } from "redux-saga/effects";
 import axios from "axios";
+// 리덕스 사가를 사용하여 서버와 데이터 통신하도록 구성하였습니다.
+// 리덕스 사가를 사용하기 위해서는 제너레이터 문법을 사용하였습니다.
+// 제너레이터 함수를 만들 때는 function* 로 시작합니다. 일반 함수와는 다르게 yield를 사용하여 여러 번 반환할 수 있습니다.
+// takeLatest는 같은 종류의 액션이 여러 번 요청된다면 가장 마지막 액션에 대해서만 동작을 실행합니다.
+// call은 함수를 실행시켜주는 것으로 동기 실행을 해줍니다.
+// fork는 call과는 반대로 비동기로 함수를 실행합니다.
+// put은 dispatch와 동일합니다.
 
 export default function* suggestionSaga() {
 	function suggestionAPI(data) {
